b6a99a24662a43110d716006aaa882c547a684e3
Anh Tran <trananh22112001@gmail.com>
2021-03-23 11:05:55 +0700

    connect entity framework

---
+++ StoreManagement/StoreManagement/Models/DataProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManagement.Models
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+
+        public static DataProvider Instance 
+        {
+            get 
+            {
+                if (instance == null)
+                {
+                    instance = new DataProvider();
+                }
+                return instance;
+            }
+            private set => instance = value;
+        }
+
+        public STOREMANAGEMENT_Entities DB { get; set; }
+
+        private DataProvider()
+        {
+            DB = new STOREMANAGEMENT_Entities();
+        }
+    }
+}
