c4d8d06d9c945fc0b24d5e7a164843b4e1aceabf
quangaka <thanhquangq998@gmail.com>
2021-03-24 02:32:00 +0700

    Create form Login

--- StoreManagement/StoreManagement/Views/HomeWindow.xaml.cs
+++ StoreManagement/StoreManagement/Views/HomeWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MaterialDesignColors;
+using MaterialDesignThemes;
 
 namespace StoreManagement
 {
@@ -22,6 +24,11 @@ namespace StoreManagement
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            
         }
     }
 }
