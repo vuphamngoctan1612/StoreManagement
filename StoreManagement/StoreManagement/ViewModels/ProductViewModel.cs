5cacea9ef2088c1efc3d9c79bc0f0621ac64361a
Anh Tran <trananh22112001@gmail.com>
2021-04-09 11:18:37 +0700

    edit Home.xaml file

--- StoreManagement/StoreManagement/ViewModels/ProductViewModel.cs
+++ StoreManagement/StoreManagement/ViewModels/ProductViewModel.cs
@@ -1,15 +1,14 @@
-﻿using StoreManagement.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LiveCharts;
+using System.Windows.Input;
 
 namespace StoreManagement.ViewModels
 {
-    public class HomeViewModel
+    public class ProductViewModel
     {
-
+        
     }
 }
