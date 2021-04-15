9c4ca88b258f0e57a5dde241a9a307015f45fd2c
Anh Tran <trananh22112001@gmail.com>
2021-04-15 10:41:19 +0700

    doing

--- StoreManagement/StoreManagement/ViewModels/HomeViewModel.cs
+++ StoreManagement/StoreManagement/ViewModels/HomeViewModel.cs
@@ -7,24 +7,58 @@ using System.Threading.Tasks;
 using LiveCharts;
 using System.Windows.Input;
 using StoreManagement.Views;
+using System.Windows.Controls;
 
 namespace StoreManagement.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
-        public ICommand Formatter { get; set; }
-        public ICommand OpenProductsWindowCommand { get; set; }
-        public ICommand AddProductCommand { get; set; }
+        private string uid;
+
+        public ICommand SwitchTabCommand { get; set; }
+        public ICommand GetUidCommand { get; set; }
 
         public HomeViewModel()
         {
-            OpenProductsWindowCommand = new RelayCommand<Views.HomeWindow>((para) => true, (para) => OpenProductsWindow(para));
+            GetUidCommand = new RelayCommand<Button>((para) => true, (para) => uid = para.Uid);
+            SwitchTabCommand = new RelayCommand<HomeWindow>((para) => true, (para) => SwitchTab(para));
         }
 
-        void OpenProductsWindow(Views.HomeWindow para)
+        private void SwitchTab(HomeWindow para)
         {
-            para.grdMain.Visibility = System.Windows.Visibility.Hidden;
-            para.grdProduct.Visibility = System.Windows.Visibility.Visible;
+            int index = int.Parse(uid);
+
+            para.grdBody_Main.Visibility = System.Windows.Visibility.Hidden;
+            para.grdBody_Store.Visibility = System.Windows.Visibility.Hidden;
+            para.grdBody_Product.Visibility = System.Windows.Visibility.Hidden;
+            para.grdBody_Bill.Visibility = System.Windows.Visibility.Hidden;
+            para.grdBody_Report.Visibility = System.Windows.Visibility.Hidden;
+            para.grdBody_AccountSetting.Visibility = System.Windows.Visibility.Hidden;
+
+
+            switch (index)
+            {
+                case 0:
+                    para.grdBody_Main.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case 1:
+                    para.grdBody_Store.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case 2:
+                    para.grdBody_Product.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case 3:
+                    para.grdBody_Bill.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case 4:
+                    para.grdBody_Report.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case 5:
+                    para.grdBody_AccountSetting.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
