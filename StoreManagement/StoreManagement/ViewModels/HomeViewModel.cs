aac35ad1e1b07bfe53356c0456187cf68d83d564
Anh Tran <trananh22112001@gmail.com>
2021-04-16 10:51:10 +0700

    dang lam

--- StoreManagement/StoreManagement/ViewModels/HomeViewModel.cs
+++ StoreManagement/StoreManagement/ViewModels/HomeViewModel.cs
@@ -8,6 +8,7 @@ using LiveCharts;
 using System.Windows.Input;
 using StoreManagement.Views;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace StoreManagement.ViewModels
 {
@@ -26,6 +27,9 @@ namespace StoreManagement.ViewModels
 
         private void SwitchTab(HomeWindow para)
         {
+            SolidColorBrush transparent = new SolidColorBrush();
+            transparent.Color = Color.FromArgb(0, 0, 0, 0);
+
             int index = int.Parse(uid);
 
             para.grdBody_Main.Visibility = System.Windows.Visibility.Hidden;
@@ -35,26 +39,38 @@ namespace StoreManagement.ViewModels
             para.grdBody_Report.Visibility = System.Windows.Visibility.Hidden;
             para.grdBody_AccountSetting.Visibility = System.Windows.Visibility.Hidden;
 
+            para.rec_btn_Menu_Main.Fill = transparent;
+            para.rec_btn_Menu_Store.Fill = transparent;
+            para.rec_btn_Menu_Product.Fill = transparent;
+            para.rec_btn_Menu_Bill.Fill = transparent;
+            para.rec_btn_Menu_Report.Fill = transparent;
+            para.rec_btn_Menu_Setting.Fill = transparent;
 
             switch (index)
             {
                 case 0:
                     para.grdBody_Main.Visibility = System.Windows.Visibility.Visible;
+                    para.rec_btn_Menu_Main.Fill = (Brush)new BrushConverter().ConvertFrom("#FF9E9F");
                     break;
                 case 1:
                     para.grdBody_Store.Visibility = System.Windows.Visibility.Visible;
+                    para.rec_btn_Menu_Store.Fill = (Brush)new BrushConverter().ConvertFrom("#FF9E9F");
                     break;
                 case 2:
                     para.grdBody_Product.Visibility = System.Windows.Visibility.Visible;
+                    para.rec_btn_Menu_Product.Fill = (Brush)new BrushConverter().ConvertFrom("#FF9E9F");
                     break;
                 case 3:
                     para.grdBody_Bill.Visibility = System.Windows.Visibility.Visible;
+                    para.rec_btn_Menu_Bill.Fill = (Brush)new BrushConverter().ConvertFrom("#FF9E9F");
                     break;
                 case 4:
                     para.grdBody_Report.Visibility = System.Windows.Visibility.Visible;
+                    para.rec_btn_Menu_Report.Fill = (Brush)new BrushConverter().ConvertFrom("#FF9E9F");
                     break;
                 case 5:
                     para.grdBody_AccountSetting.Visibility = System.Windows.Visibility.Visible;
+                    para.rec_btn_Menu_Setting.Fill = (Brush)new BrushConverter().ConvertFrom("#FF9E9F");
                     break;
                 default:
                     break;
