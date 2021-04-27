48979f08ef0b5d9a05d3651192ce35fc73f50f5e
Anh Tran <trananh22112001@gmail.com>
2021-04-28 00:41:39 +0700

    doing

--- AgencyManagement.sql
+++ AgencyManagement.sql
@@ -105,3 +105,95 @@ go
 
 alter table Agency add constraint FK_Type foreign key (TypeOfAgency) references TypeOfAgency(ID)
 go
+
+
+
+-- insert
+insert into TypeOfAgency values (1, 'Type 1', 1000000)
+insert into TypeOfAgency values (2, 'Type 2', 2000000)
+
+insert into Agency values (1, 'Agency 1', 1, '0987654321', 'TP HCM', 'Quan 1', GETDATE(), 'abcdxyz@gmail.com', 500000, 0)
+insert into Agency values (2, 'Agency 2', 2, '0987234321', 'TP HCM', 'Quan 1', GETDATE(), 'abcdxyz@gmail.com', 600000, 0)
+insert into Agency values (3, 'Agency 3', 2, '0985314321', 'TP HCM', 'Quan 1', GETDATE(), 'abcdxyz@gmail.com', 800000, 0)
+insert into Agency values (4, 'Agency 4', 1, '0987844321', 'TP HCM', 'Quan 1', GETDATE(), 'abcdxyz@gmail.com', 900000, 0)
+
+select * from InvoiceInfo
+select * from Invoice
+select * from Product
+
+
+insert into Invoice values (1,1,GETDATE(), 200000)
+insert into Invoice values (2,1,GETDATE(), 340000)
+insert into Invoice values (3,1,GETDATE(), 6100000)
+
+insert into Invoice values (4,2,GETDATE(), 860000)
+insert into Invoice values (5,2,GETDATE(), 900000)
+insert into Invoice values (6,2,GETDATE(), 640000)
+insert into Invoice values (7,2,GETDATE(), 2500000)
+
+insert into Invoice values (8,3,GETDATE(), 700000)
+insert into Invoice values (9,3,GETDATE(), 900000)
+insert into Invoice values (10,3,GETDATE(), 550000)
+insert into Invoice values (11,3,GETDATE(), 1000000)
+
+insert into Invoice values (12,4,GETDATE(), 2000000)
+insert into Invoice values (13,4,GETDATE(), 210000)
+insert into Invoice values (14,4,GETDATE(), 500000)
+insert into Invoice values (15,4,GETDATE(), 200000)
+insert into Invoice values (16,4,'2021-2-19', 290000)
+
+--GET DAYS IN MONTH
+SELECT DAY(CHECKOUT) AS DAY FROM Invoice
+WHERE MONTH(CHECKOUT) = 4 AND YEAR(CHECKOUT) = 2021
+GROUP BY DAY(CHECKOUT)
+-- GET MONTH IN YEAR
+SELECT MONTH(CHECKOUT) AS MONTH FROM Invoice
+WHERE YEAR(CHECKOUT) = 2021
+GROUP BY MONTH(CHECKOUT)
+--GET QUARTER IN YEAR
+SELECT DATEPART(QUARTER, CHECKOUT) AS QUARTER FROM Invoice
+WHERE YEAR(CHECKOUT) = 2021
+GROUP BY DATEPART(QUARTER, CHECKOUT)
+
+
+-- GET DEBT BY YEAR
+SELECT SUM(DEBT) AS TOTAL FROM Invoice
+WHERE YEAR(CHECKOUT) = 2021
+GROUP BY MONTH(CHECKOUT)
+-- GET DEBT BY QUARTER
+SELECT SUM(Debt) AS TOTAL FROM Invoice 
+WHERE YEAR(CHECKOUT) = 2021
+GROUP BY DATEPART(QUARTER, CHECKOUT)
+-- GET DEBT BY DAYS
+SELECT DAY(CHECKOUT) AS DAY, SUM(Debt) AS TOTAL FROM Invoice
+WHERE MONTH(CHECKOUT) = 2 AND YEAR(CHECKOUT) = 2021
+GROUP BY DAY(CHECKOUT)
+
+SELECT * FROM Invoice
+SELECT * FROM InvoiceInfo
+SELECT * FROM Product
+
+SELECT SUM(Total) FROM InvoiceInfo
+JOIN Invoice ON InvoiceInfo.InvoiceID = Invoice.ID
+WHERE MONTH(Checkout) = 4 AND YEAR(CHECKOUT) = 2021
+GROUP BY DAY(CHECKOUT)
+
+SELECT SUM(Total) FROM InvoiceInfo
+JOIN Invoice ON InvoiceInfo.InvoiceID = Invoice.ID
+WHERE YEAR(CHECKOUT) = 2021
+GROUP BY MONTH(Checkout)
+
+SELECT SUM(Total) FROM InvoiceInfo
+JOIN Invoice ON InvoiceInfo.InvoiceID = Invoice.ID
+WHERE YEAR(CHECKOUT) = 2021
+GROUP BY DATEPART(QUARTER, CHECKOUT)
+
+INSERT INTO InvoiceInfo VALUES (1, 1,3, 600000)
+INSERT INTO InvoiceInfo VALUES (1, 2,3, 450000)
+INSERT INTO InvoiceInfo VALUES (1, 3,3, 600000)
+INSERT INTO InvoiceInfo VALUES (1, 4,3, 240000)
+
+INSERT INTO InvoiceInfo VALUES (2, 1,3, 600000)
+INSERT INTO InvoiceInfo VALUES (2, 2,3, 450000)
+INSERT INTO InvoiceInfo VALUES (2, 3,3, 600000)
+INSERT INTO InvoiceInfo VALUES (2, 4,3, 1000000)
--- StoreManagement/StoreManagement/ViewModels/ReportViewModel.cs
+++ StoreManagement/StoreManagement/ViewModels/ReportViewModel.cs
@@ -1,26 +1,327 @@
-﻿using StoreManagement.Models;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using StoreManagement.Models;
 using StoreManagement.Views;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace StoreManagement.ViewModels
 {
     public class ReportViewModel : BaseViewModel
     {
-        public ICommand LoadTop4AgencyCommand { get; set; }
+        public HomeWindow HomeWindow { get; set; }
+
+        private string axisXTitle;
+        public string AxisXTitle
+        {
+            get => axisXTitle;
+            set 
+            {
+                axisXTitle = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        private string axisYTitle;
+        public string AxisYTitle
+        {
+            get => axisYTitle;
+            set
+            {
+                axisYTitle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string[] labels;
+        public string[] Labels
+        {
+            get => labels;
+            set
+            {
+                labels = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Func<double, string> formatter;
+        public Func<double, string> Formatter 
+        { 
+            get => formatter;
+            set 
+            {
+                formatter = value; 
+                OnPropertyChanged();
+            } 
+        }
+
+        private SeriesCollection seriesCollection;
+        public SeriesCollection SeriesCollection
+        {
+            get => seriesCollection;
+            set
+            {
+                seriesCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LoadTop3AgencyCommand { get; set; }
+        public ICommand InitColumnChartCommand { get; set; }
 
         public ReportViewModel()
         {
-            LoadTop4AgencyCommand = new RelayCommand<HomeWindow>((para) => true, (para) => LoadTop4Agency(para));
+            LoadTop3AgencyCommand = new RelayCommand<HomeWindow>((para) => true, (para) => LoadTop3Agency(para));
+            InitColumnChartCommand = new RelayCommand<HomeWindow>((para) => true, (para) => InitColumnChart(para));
         }
 
-        private void LoadTop4Agency(HomeWindow para)
+        private void InitColumnChart(HomeWindow para)
+        {
+            AxisXTitle = "Days";
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Revenue",
+                    Values = this.GetTotalByQuarter("2021")
+                },
+                new ColumnSeries
+                {
+                    Title  = "Debt",
+                    Values = this.GetDebtByQuarter("2021")
+                }
+            };
+            Labels = this.GetQuarterInYear(DateTime.Now.Year.ToString());
+            Formatter = value => string.Format("{0:N0}", value);
+        }
+
+        private void LoadTop3Agency(HomeWindow para)
         {
 
+        }
+
+        //ReportDAL
+        private string[] GetDayInMonth(string month, string year)
+        {
+            List<string> res = new List<string>();
+            List<Int32> temp = new List<Int32>();
+
+            try
+            {
+                string query = string.Format("SELECT DAY(CHECKOUT) AS DAY FROM Invoice " +
+                    "WHERE MONTH(CHECKOUT) = {0} AND YEAR(CHECKOUT) = {1} " +
+                    "GROUP BY DAY(CHECKOUT)", month, year);
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int32>(query).ToList();
+                foreach (var item in temp)
+                {
+                    res.Add(item.ToString());
+                }
+                return res.ToArray();
+            }
+            catch
+            {
+                return res.ToArray();
+            }
+        }
+        private string[] GetMonthInYear(string year)
+        {
+            List<string> res = new List<string>();
+            List<Int32> temp = new List<Int32>();
+
+            try
+            {
+                string query = string.Format("SELECT MONTH(CHECKOUT) AS MONTH FROM Invoice " +
+                        "WHERE YEAR(CHECKOUT) = {0} " +
+                        "GROUP BY MONTH(CHECKOUT)", year);
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int32>(query).ToList();
+
+                foreach (Int32 item in temp)
+                {
+                    res.Add(item.ToString());
+                }
+                return res.ToArray();
+            }
+            catch
+            {
+                return res.ToArray();
+            }
+        }
+        private string[] GetQuarterInYear(string year)
+        {
+            List<string> res = new List<string>();
+            List<Int32> temp = new List<Int32>();
+
+            try
+            {
+                string query = string.Format("SELECT DATEPART(QUARTER, CHECKOUT) AS QUARTER FROM Invoice " +
+                    "WHERE YEAR(CHECKOUT) = {0} " +
+                    "GROUP BY DATEPART(QUARTER, CHECKOUT)", year);
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int32>(query).ToList();
+
+                foreach (Int32 item in temp)
+                {
+                    res.Add(item.ToString());
+                }
+                return res.ToArray();
+            }
+            catch
+            {
+                return res.ToArray();
+            }
+        }
+
+        private ChartValues<double> GetDebtByMonth(string month, string year)
+        {
+            ChartValues<double> res = new ChartValues<double>();
+            List<Int64> temp = new List<Int64>();
+
+            try
+            {
+                string query = string.Format("SELECT SUM(Debt) AS TOTAL FROM Invoice " +
+                    "WHERE MONTH(CHECKOUT) = {0} AND YEAR(CHECKOUT) = {1} " +
+                    "GROUP BY DAY(CHECKOUT)", month, year);
+
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int64>(query).ToList();
+
+                foreach (Int64 item in temp)
+                {
+                    res.Add((double.Parse(item.ToString())));
+                }
+                return res;
+            }
+            catch
+            {
+                return res;
+            }
+        }
+        private ChartValues<double> GetDebtByYear(string year)
+        {
+            ChartValues<double> res = new ChartValues<double>();
+            List<Int64> temp = new List<Int64>();
+
+            try
+            {
+                string query = string.Format("SELECT SUM(DEBT) AS TOTAL FROM Invoice " +
+                    "WHERE YEAR(CHECKOUT) = {0} " +
+                    "GROUP BY MONTH(CHECKOUT)", year);
+
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int64>(query).ToList();
+
+                foreach (Int64 item in temp)
+                {
+                    res.Add((double.Parse(item.ToString())));
+                }
+                return res;
+            }
+            catch
+            {
+                return res;
+            }
+        }
+        private ChartValues<double> GetDebtByQuarter(string year)
+        {
+            ChartValues<double> res = new ChartValues<double>();
+            List<Int64> temp = new List<Int64>();
+
+            try
+            {
+                string query = string.Format("SELECT SUM(Debt) AS TOTAL FROM Invoice " +
+                    "WHERE YEAR(CHECKOUT) = {0} " +
+                    "GROUP BY DATEPART(QUARTER, CHECKOUT)", year);
+
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int64>(query).ToList();
+
+                foreach (Int64 item in temp)
+                {
+                    res.Add((double.Parse(item.ToString())));
+                }
+                return res;
+            }
+            catch
+            {
+                return res;
+            }
+        }
+
+        private ChartValues<double> GetTotalByMonth(string month, string year)
+        {
+            ChartValues<double> res = new ChartValues<double>();
+            List<Int64> temp = new List<Int64>();
+
+            try
+            {
+                string query = string.Format("SELECT SUM(Total) FROM InvoiceInfo " +
+                    "JOIN Invoice ON InvoiceInfo.InvoiceID = Invoice.ID " +
+                    "WHERE MONTH(Checkout) = {0} AND YEAR(CHECKOUT) = {1} " +
+                    "GROUP BY DAY(CHECKOUT)", month, year);
+
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int64>(query).ToList();
+
+                foreach (Int64 item in temp)
+                {
+                    res.Add((double.Parse(item.ToString())));
+                }
+                return res;
+            }
+            catch
+            {
+                return res;
+            }
+        }
+        private ChartValues<double> GetTotalByYear(string year)
+        {
+            ChartValues<double> res = new ChartValues<double>();
+            List<Int64> temp = new List<Int64>();
+
+            try
+            {
+                string query = string.Format("SELECT SUM(Total) FROM InvoiceInfo " +
+                    "JOIN Invoice ON InvoiceInfo.InvoiceID = Invoice.ID " +
+                    "WHERE YEAR(CHECKOUT) = {0} " +
+                    "GROUP BY MONTH(Checkout)", year);
+
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int64>(query).ToList();
+
+                foreach (Int64 item in temp)
+                {
+                    res.Add((double.Parse(item.ToString())));
+                }
+                return res;
+            }
+            catch
+            {
+                return res;
+            }
+        }
+        private ChartValues<double> GetTotalByQuarter(string year)
+        {
+            ChartValues<double> res = new ChartValues<double>();
+            List<Int64> temp = new List<Int64>();
+
+            try
+            {
+                string query = string.Format("SELECT SUM(Total) FROM InvoiceInfo " +
+                    "JOIN Invoice ON InvoiceInfo.InvoiceID = Invoice.ID " +
+                    "WHERE YEAR(CHECKOUT) = {0} " +
+                    "GROUP BY DATEPART(QUARTER, CHECKOUT)", year);
+                temp = DataProvider.Instance.DB.Database.SqlQuery<Int64>(query).ToList();
+
+                foreach (Int64 item in temp)
+                {
+                    res.Add((double.Parse(item.ToString())));
+                }
+                return res;
+            }
+            catch
+            {
+                return res;
+            }
         }
     }
 }
